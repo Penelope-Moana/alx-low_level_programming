0e1f9223e87fb0003467aecf3c8cd55fd285e22b
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-10 14:27:42 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main(int argc, char** argv)
+
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
+
+}
+
