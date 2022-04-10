91552c8042668eef6ba80d26adca88adb9f24879
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-10 15:30:01 -0700

    quote correction 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (stderr)
  */
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long 1 = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 1;
-__asm__ ("syscall"
-		: "=a" (ret),
-		: "a" (syscall),
-		:"D" (fd),
-		:"S" (s),
-		:"d" (1),
-	);
-
-return (0);
-
-}
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);}
 
