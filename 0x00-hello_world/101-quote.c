17af2226ee35d1196a6c71af12ffdebb437335f2
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-10 14:28:58 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 
 int write(int filedes, const char *buf, unsigned int nbyte);
-int main(int argc, char** argv)
+int main(int argc, char** argv);
 
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
