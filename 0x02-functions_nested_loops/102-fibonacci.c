72bb2884614dd50588dd50ce8137c264223dbf23
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-13 10:32:26 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 FIbonacci numbers
+ *
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0;
+	for (i = 0; i < 49; i++)
+	{
+		printf("%1d, ", x);
+
+		sum = x + y;
+		x = y;
+		y = sum;
+		if (i == 48)
+			printf("%1d\n", x);
+	}
+	return (0);
+
+}
