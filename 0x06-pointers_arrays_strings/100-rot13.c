6a8130b906eb219616f83c928d5ea1acb7ecec7c
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-23 12:59:36 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot 13 - encodes strings using rot 13.
+ * @s: pointer to string.
+ *
+ * Return: pointer to encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; input[y] != '\0'; y++)
+		{
+			if (s[x] == input[y])
+			{
+				s[x] = output[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
