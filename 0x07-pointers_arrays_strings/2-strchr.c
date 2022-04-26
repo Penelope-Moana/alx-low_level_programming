67a52b911e742acdbef12bc6a5853a9c4b88ce58
Penelope Mashapa <mmoya1306@gmail.com>
2022-04-26 13:53:19 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,14 @@ char *_strchr(char *s, char c)
 {
 	int a= 0, b;
 
-	while (s[a]
+	while (s[a])
 	{
 		a++;
 	}
 
 	for (b = 0; b <= a; b++)
 	{
-		if (c == s[b]
+		if (c == s[b])
 		{
 			s += b;
 			return (s);
